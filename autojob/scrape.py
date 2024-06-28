0f406aa5ddb256a2c157ccf71faef1d72598f9b6
Stephen Kent <smkent@smkent.net>
2024-06-27 21:14:18 -0700

    Start scrape updates for moving to API

--- autojob/scrape.py
+++ autojob/scrape.py
@@ -9,10 +9,9 @@ from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
 from flatten_json import flatten as flatten_json  # type: ignore
-from pandas import DataFrame  # type: ignore
 
+from .api import Company, api_client
 from .posting import Posting, posting_factory
-from .spreadsheet import Spreadsheet
 
 
 @dataclass
@@ -53,6 +52,9 @@ class CareersPageParser:
 
 
 class Scrape:
+    def __init__(self) -> None:
+        api_client.load_postings()
+
     @cached_property
     def args(self) -> Namespace:
         ap = ArgumentParser(description="Job posting scraper")
@@ -116,16 +118,7 @@ class Scrape:
             )
             for company_url in company_urls:
                 for url in self.page_urls(company_url):
-                    try:
-                        df = self.postings_sheet.loc[
-                            self.postings_sheet["Role Posting URL"].str.match(
-                                url, na=False
-                            )
-                        ]
-                    except Exception as e:
-                        print(f"Error checking if {url} exists: {e}")
-                        continue
-                    if not df.empty:
+                    if url in api_client.postings_by_url:
                         continue
                     if self.args.verbose:
                         print("Consider URL", url)
@@ -147,30 +140,17 @@ class Scrape:
                 else:
                     yield self.company_urls(company)
             return
-        for row in self.companies_sheet.iterrows():
-            row_data = self.companies_sheet.iloc[row[0]]
-            yield self.company_row_to_urls(row_data)
+        for company in api_client.companies_by_name.keys():
+            yield self.company_urls(company)
 
     def company_urls(self, company: str) -> tuple[str, Sequence[str]]:
-        df = self.companies_sheet.loc[
-            self.companies_sheet["Company"].str.match(
-                company.lower(), na=False, case=False
-            )
-        ]
-        if df["Company"].count() < 1:
-            raise Exception(f"No spreadsheet row found for {company}")
-        for row in df.iterrows():
-            row_data = self.companies_sheet.iloc[row[0]]
-            return self.company_row_to_urls(row_data)
-        raise Exception(f"No careers page URL found for {company}")
+        def _c_urls(instance: Company) -> Sequence[str]:
+            return [instance.careers_url] + (instance.careers_urls or [])
 
-    def company_row_to_urls(self, row: DataFrame) -> tuple[str, Sequence[str]]:
-        urls = [row["Careers Pages"]]
-        if (additional_urls := row["Additional Careers URLs"]) and isinstance(
-            additional_urls, str
-        ):
-            urls += [u.strip() for u in additional_urls.split(",")]
-        return row["Company"], urls
+        try:
+            return company, _c_urls(api_client.companies_by_name[company])
+        except KeyError:
+            raise Exception(f'No company "{company}" found')
 
     def page_urls(self, url: str) -> Sequence[str]:
         urls = []
@@ -183,11 +163,3 @@ class Scrape:
         except (requests.exceptions.HTTPError, Exception) as e:
             print(f"Unable to retrieve {url}, skipping ({e})")
         return urls
-
-    @cached_property
-    def companies_sheet(self) -> DataFrame:
-        return Spreadsheet.sheet("Companies")
-
-    @cached_property
-    def postings_sheet(self) -> DataFrame:
-        return Spreadsheet.sheet("Postings")
